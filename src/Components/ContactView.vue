0aeaed350782c07f13b608e91b16ad840f042229
Syam-18 <sukeshgrandhisila@gmail.com>
2025-11-25 12:12:41 +0530

    contact section width and icons visibility

--- src/Components/ContactView.vue
+++ src/Components/ContactView.vue
@@ -2,19 +2,19 @@
 
 </script>
 <template>
-  <form id="contact" class="flex flex-col justify-center items-start w-[60vw] h-[40vh] m-12">
+  <form id="contact" class="flex flex-col justify-center items-start w-[80vw] md:w-[60vw] h-[40vh] m-12">
     <div class="flex items-center gap-2">
       <a class="text-3xl font-medium text-[#fcd262]" href="#contact">CONTACT</a>
         <i class="fa-solid fa-paper-plane text-[#fcfa8b] mt-2"></i>
       </div>
       <div class="flex gap-2 mt-4">
-        <a href="https://www.linkedin.com/in/syam-sukesh-grandhisila/" target='blank' class="p-3 hover:bg-amber-100 rounded">
+        <a href="https://www.linkedin.com/in/syam-sukesh-grandhisila/" target='blank' class="p-3 hover:bg-amber-100 bg-amber-100 md:bg-none rounded">
           <i class="fa-brands fa-linkedin"></i>
         </a>
-        <a href="https://www.github.com/Syam-18" target='blank' class="hover:bg-amber-100 rounded p-3">
+        <a href="https://www.github.com/Syam-18" target='blank' class="hover:bg-amber-100 bg-amber-100 md:bg-none rounded p-3">
           <i class="fa-brands fa-github "></i>
         </a>
-        <a href="mailto:sukeshgrandhisila@gmail.com" class="hover:bg-amber-100 rounded p-3">
+        <a href="mailto:sukeshgrandhisila@gmail.com" class="hover:bg-amber-100 bg-amber-100 md:bg-none rounded p-3">
           <i class="fa-solid fa-envelope "></i>
         </a>
       </div>
